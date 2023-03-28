eed15d6bc864b0303c07befbc5a1d68b8c81d051
dattatreyav30 <dattatreya361@gmail.com>
2023-03-28 10:18:31 +0530

    adding unique email id feature

--- models/userModel.js
+++ models/userModel.js
@@ -15,7 +15,8 @@ const User = sequelize.define('user', {
     },
     email: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
+        unique : true
     },
     password: {
         type: Sequelize.STRING,
